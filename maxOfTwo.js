fa7c5c274dfd1cc1172e36e05e8d9f8882bfe7be
Michael Nowlin <michaelnowlin5@gmail.com>
2022-02-17 20:18:30 -0700

    Initial commit

---
+++ maxOfTwo.js
@@ -0,0 +1,11 @@
+let x = 1
+let y = 2
+
+// Write your code below this line.
+if(x > y){
+    console.log(x);
+} else if(x < y){
+    console.log(y);
+} else {
+    console.log('The Values are identical');
+}
